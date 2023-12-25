73d86ad980ccd723e6b77f6d7b50122c35792ed9
Daniel Luque <danielluque14@gmail.com>
2023-12-25 23:10:59 +0100

    Pass the initial_prompt option as context to DeepL

--- whisper2subs/translate.py
+++ whisper2subs/translate.py
@@ -1,23 +1,30 @@
-import deepl
+from deepl import DeepLException, Translator
 
 from whisper2subs.transcribe import TranscribeResult
 
 
 def deepl_translate(
-    transcribed_audio: TranscribeResult, output_language: str, api_key: str
+    transcribed_audio: TranscribeResult,
+    output_language: str,
+    api_key: str,
+    context: str,
 ) -> TranscribeResult:
     dialogues = [x["text"] for x in transcribed_audio["segments"]]  # type: ignore
 
     try:
-        translator = deepl.Translator(api_key, send_platform_info=False)
+        translator = Translator(api_key, send_platform_info=False)
         result = translator.translate_text(
-            dialogues, source_lang="EN", target_lang=output_language.upper()
+            dialogues,
+            source_lang="EN",
+            target_lang=output_language.upper(),
+            context=context,
         )
-    except deepl.DeepLException as err:
+    except DeepLException as err:
         raise err
 
     if not isinstance(result, list):
         result = [result]
     for index, line in enumerate(result, 0):
         transcribed_audio["segments"][index]["text"] = line.text  # type: ignore
+
     return transcribed_audio
